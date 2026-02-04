7a14ddd0afec9bbd828f05e8f122353b3011bbf3
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-04 11:23:00 +0530

    add dotenv and cors

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

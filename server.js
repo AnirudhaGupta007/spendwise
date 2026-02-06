3941f91f82cefb65e9a53b7eeed2f91a892c6b84
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-06 10:49:00 +0530

    connect db in server

--- server.js
+++ server.js
@@ -8,5 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+const connectDB = require("./config/db");
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

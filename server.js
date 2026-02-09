74dc157112fe70e009872715b291cfb28a413e32
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-09 12:45:00 +0530

    register user routes in server

--- server.js
+++ server.js
@@ -11,5 +11,7 @@ app.use(morgan("dev"));
 const connectDB = require("./config/db");
 connectDB();
 
+app.use("/api/users", require("./routes/userRoutes"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

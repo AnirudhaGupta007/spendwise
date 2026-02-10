10e0ec87ac47e4bc87ac72275648c0f0489a9a75
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-10 15:06:00 +0530

    add category constants

---
+++ config/categories.js
@@ -0,0 +1,4 @@
+module.exports = {
+  incomeCategories: ["salary", "freelance", "investment", "gift", "other"],
+  expenseCategories: ["food", "rent", "transport", "shopping", "bills", "entertainment", "health", "education", "other"],
+};

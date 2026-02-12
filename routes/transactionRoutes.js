69884fff6caba5d191db3786ddf76bb825dca958
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-12 10:34:00 +0530

    add summary route

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const { getTransactions, addTransaction, deleteTransaction, updateTransaction } = require("../controllers/transactionController");
+const { getTransactions, addTransaction, deleteTransaction, updateTransaction, getSummary } = require("../controllers/transactionController");
 
 router.get("/", auth, getTransactions);
+router.get("/summary", auth, getSummary);
 router.post("/", auth, addTransaction);
 router.delete("/:id", auth, deleteTransaction);
 router.put("/:id", auth, updateTransaction);

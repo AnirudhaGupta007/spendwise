d27663606badb02362495892e787ed58f696662e
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-17 14:50:00 +0530

    add protected route

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
+  const isLoggedIn = () => !!localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={isLoggedIn() ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>

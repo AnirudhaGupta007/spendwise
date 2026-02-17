2892d0b173189bb51a1f93b3a7d9becbbb161c3c
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-17 12:29:00 +0530

    create home page skeleton

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <div className="home-page">
+        <h2>Dashboard</h2>
+        <p>Your expense overview will appear here</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;

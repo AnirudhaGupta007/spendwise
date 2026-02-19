e9998e3b907b1be0ec539b483e3d09a28bdd5caf
Anirudha Gupta <gupta.anirudha007@gmail.com>
2026-02-19 14:18:00 +0530

    add transaction page route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HomePage from "./pages/HomePage";
+import AddTransaction from "./pages/AddTransaction";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/add" element={isLoggedIn() ? <AddTransaction /> : <Navigate to="/login" />} />
         <Route path="/" element={isLoggedIn() ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
